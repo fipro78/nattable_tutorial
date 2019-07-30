c56920e50def3d2e2de5fd8c7ca860cff446a640
Dirk Fauth <dirk.fauth@googlemail.com>
2019-07-30 11:59:49 +0200

    Updated steps in extended exercise

--- com.vogella.nattable/src/com/vogella/nattable/parts/NatTableExtendedExamplePart.java
+++ com.vogella.nattable/src/com/vogella/nattable/parts/NatTableExtendedExamplePart.java
@@ -16,15 +16,20 @@ public class NatTableExtendedExamplePart {
 		// create the layout
 		parent.setLayout(new GridLayout());
 		
-		// create the ReflectiveColumnPropertyAccessor
+		// create the ExtendedReflectiveColumnPropertyAccessor for ExtendedPersonWithAddress
 		
 		// create a ListDataProvider
 		
-		// create a body layer stack
+		// create a body layer stack with a sorted collection by last name
 		
 		// create a column header layer stack with column grouping
+		
+		// configure the column groups
 
 		// create a row header layer stack with row grouping
+		
+		// configure the row groups
+		// collect containing last names and the number of persons with that last name
 
 		// create a corner layer stack
 		
@@ -33,8 +38,17 @@ public class NatTableExtendedExamplePart {
 		// create the NatTable instance using the GridLayer instance
 		NatTable natTable = null;
 		
+		// register a column group header menu
+
+		// register a row group header menu
+
+		// register display persistence dialog
+		
+		// register column chooser
+		
 		// set the layout data
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(natTable);
 	}
 	
+	// create an AbstractLayerTransform to encapsulate the body layer stack
 }
